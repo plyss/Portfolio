728c32a2180ddc1915c99e44e941ded78565eb16
plyss <mfaisalalam13@gmail.com>
2023-02-19 11:02:38 +0700

    update

--- jdm-car-information-react/functions/api.js
+++ jdm-car-information-react/functions/api.js
@@ -1,10 +1,25 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('../db.json');
-const middlewares = jsonServer.defaults();
+exports.handler = async (event, context) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  };
 
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log('Server is running');
-});
+  // Add headers to the response
+  const response = {
+    headers: headers,
+    statusCode: 200,
+    body: ""
+  };
+
+  const jsonServer = require('json-server');
+  const server = jsonServer.create();
+  const router = jsonServer.router('../db.json');
+  const middlewares = jsonServer.defaults();
+
+  server.use(middlewares);
+  server.use(router);
+  server.listen(3000, () => {
+    console.log('Server is running');
+  });
+}
+
